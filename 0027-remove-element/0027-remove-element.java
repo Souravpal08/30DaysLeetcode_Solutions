629ea269cafd1431b0759e882607e28da1498eef
Sourav Pal <143326384+Souravpal08@users.noreply.github.com>
2024-12-02 22:11:54 +0530

    add comment in 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -7,10 +7,12 @@ class Solution {
 
     }
     public int removeElement(int[] nums, int val) {
+        //pointer starts at the last index of the array (nums.length - 1).
         int pointer = nums.length-1;
         int i=0;
         while(i <= pointer){
             if(nums[i]==val){
+                //calling swap function for if val is matched with i
                 swap(nums, i, pointer);
                     pointer --;
             } else {
